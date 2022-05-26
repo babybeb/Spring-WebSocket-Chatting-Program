a144225832d7617b643501300818e9babdfffd8c
babybeb <dasomchoi52@gmail.com>
2022-05-26 23:26:31 +0900

    Add WebSocketConfig

---
+++ src/main/java/babybeb/chattingservice/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package babybeb.chattingservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+    
+    private final WebSocketHandler webSocketHandler;
+    
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+            .addHandler(webSocketHandler, "ws/chat")
+            .setAllowedOrigins("*");
+    }
+}
