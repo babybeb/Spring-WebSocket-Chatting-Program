415c9498b83f8e8efe1c58b61f4ea700c140a881
babybeb <dasomchoi52@gmail.com>
2022-05-26 23:25:52 +0900

    Fix ChattingServiceApplication to exclude DataSourceAutoConfiguration.class

--- src/main/java/babybeb/chattingservice/ChattingServiceApplication.java
+++ src/main/java/babybeb/chattingservice/ChattingServiceApplication.java
@@ -2,12 +2,13 @@ package babybeb.chattingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ChattingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChattingServiceApplication.class, args);
 	}
-
 }
